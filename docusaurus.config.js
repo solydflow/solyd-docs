53ea3b3e0181ed778faed6f085ab4db81cd9a342
miracle-a-osigwe <miracle.osigwe6@gmail.com>
2026-06-29 13:50:52 +0100

    Update docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/',
+          // routeBasePath: '/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
